fa6b13d49cde900d56a886e0229319ab71002ff7
Jim Wang <jim.wang@corva.ai>
2020-07-24 14:46:16 -0500

    update output to include dig timeouts

--- today_slow.sh
+++ today_slow.sh
@@ -2,5 +2,6 @@
 
 (cat /usr/local/lib/netstat/logs/$(date +%F)/fping.*.log | grep ": -" | awk '{print $2,$3,$4,$10,$8}';
 cat /usr/local/lib/netstat/logs/$(date +%F)/fping.*.log | sort -rn -k10,10 | head -n 20 | awk '{print $2,$3,$4,$10,$8}';
+cat /usr/local/lib/netstat/logs/$(date +%F)/dig.*.log | grep 'timed out' | awk '{print $2,$3,$4,$10,$11}';
 cat /usr/local/lib/netstat/logs/$(date +%F)/dig.*.log | sort -rn -k11,11 | head -n 20 | awk '{print $2,$3,$4,$11,$9}') |  sort -n -k3,3
 
--- yesterday_slow.sh
+++ yesterday_slow.sh
@@ -2,4 +2,5 @@
 
 (cat /usr/local/lib/netstat/logs/$(date -d "24 hours ago" +%F)/fping.*.log | grep ": -" | awk '{print $2,$3,$4,$10,$8}';
 cat /usr/local/lib/netstat/logs/$(date -d "24 hours ago" +%F)/fping.*.log | sort -rn -k10,10 | head -n 10 | awk '{print $2,$3,$4,$10,$8}';
+cat /usr/local/lib/netstat/logs/$(date -d "24 hours ago" +%F)/dig.*.log | grep 'timed out' | awk '{print $2,$3,$4,$10,$11}';
 cat /usr/local/lib/netstat/logs/$(date -d "24 hours ago" +%F)/dig.*.log | sort -rn -k11,11 | head -n 10 | awk '{print $2,$3,$4,$11,$9}') |  sort -n -k3,3
