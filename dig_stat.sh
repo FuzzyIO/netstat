b333e7b72d9d23e2ae8f90b3fa344598f2d038a6
Jim Wang <jim.wang@corva.ai>
2020-07-24 22:31:24 -0500

    update dig logic to include ip inline

--- dig_stat.sh
+++ dig_stat.sh
@@ -8,7 +8,7 @@ ip_index=$(( $(expr $(date +%s) + $RANDOM) % ${#ip_addresses[@]} ))
 target_ip=${ip_addresses[$ip_index]}
 
 ## Run this for an hour, lookup every 3 seconds
-for i in {1..1200}; do /usr/bin/dig +time=1 @$target_ip google.com | grep 'timed out\|Query' | while read line; do echo `date` - $line; done; sleep 3; done >> $FILE_NAME
+for i in {1..1200}; do /usr/bin/dig +time=1 @$target_ip google.com | grep 'timed out\|Query' | while read line; do echo `date` - $target_ip - $line; done; sleep 3; done >> $FILE_NAME
 
 ## Clear logs from 30 days ago
 find ${ROOT_DIR}/ -ctime +30 -delete
--- today_slow.sh
+++ today_slow.sh
@@ -2,6 +2,6 @@
 
 (cat /usr/local/lib/netstat/logs/$(date +%F)/fping.*.log | grep ": -" | awk '{print $2,$3,$4,$10,$8}';
 cat /usr/local/lib/netstat/logs/$(date +%F)/fping.*.log | sort -rn -k10,10 | head -n 20 | awk '{print $2,$3,$4,$10,$8}';
-cat /usr/local/lib/netstat/logs/$(date +%F)/dig.*.log | grep 'timed out' | awk '{print $2,$3,$4,$10,$11}';
-cat /usr/local/lib/netstat/logs/$(date +%F)/dig.*.log | sort -rn -k11,11 | head -n 20 | awk '{print $2,$3,$4,$11,$9}') |  sort -n -k3,3
+cat /usr/local/lib/netstat/logs/$(date +%F)/dig.*.log | grep 'timed out' | awk '{print $2,$3,$4,$12,$13,$8}';
+cat /usr/local/lib/netstat/logs/$(date +%F)/dig.*.log | sort -rn -k13,13 | head -n 20 | awk '{print $2,$3,$4,$13,$8,$11}') |  sort -n -k3,3
 
