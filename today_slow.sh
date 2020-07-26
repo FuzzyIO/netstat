b6019b00b38c4927ed481ee2d7c14f65ca149a1c
Jim Wang <jim.wang@corva.ai>
2020-07-25 22:18:29 -0500

    refactor the log output script

--- today_slow.sh
+++ today_slow.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-(cat /usr/local/lib/netstat/logs/$(date +%F)/fping.*.log | grep ": -" | awk '{print $2,$3,$4,$10,$8}';
-cat /usr/local/lib/netstat/logs/$(date +%F)/fping.*.log | sort -rn -k10,10 | head -n 20 | awk '{print $2,$3,$4,$10,$8}';
-cat /usr/local/lib/netstat/logs/$(date +%F)/dig.*.log | grep 'timed out' | awk '{print $2,$3,$4,$12,$13,$8}';
-cat /usr/local/lib/netstat/logs/$(date +%F)/dig.*.log | sort -rn -k13,13 | head -n 20 | awk '{print $2,$3,$4,$13,$8,$11}') |  sort -n -k3,3
+ROOT_FOLDER=/usr/local/lib/netstat/logs/$(date +%F)
 
+(cat $ROOT_FOLDER/fping.*.log | grep ": -" | awk '{print $2,$3,$4,$10,$8}';
+cat $ROOT_FOLDER/fping.*.log | sort -rn -k10,10 | head -n 15 | awk '{print $2,$3,$4,$10,$8}';
+cat $ROOT_FOLDER/dig.*.log | grep 'timed out' | awk '{print $2,$3,$4,$12,$13,$8}';
+cat $ROOT_FOLDER/dig.*.log | sort -rn -k13,13 | head -n 15 | awk '{print $2,$3,$4,$13,$8,$11}') |  sort -n -k3,3
--- yesterday_slow.sh
+++ yesterday_slow.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-(cat /usr/local/lib/netstat/logs/$(date -d "24 hours ago" +%F)/fping.*.log | grep ": -" | awk '{print $2,$3,$4,$10,$8}';
-cat /usr/local/lib/netstat/logs/$(date -d "24 hours ago" +%F)/fping.*.log | sort -rn -k10,10 | head -n 10 | awk '{print $2,$3,$4,$10,$8}';
-cat /usr/local/lib/netstat/logs/$(date -d "24 hours ago" +%F)/dig.*.log | grep 'timed out' | awk '{print $2,$3,$4,$12,$13,$8}';
-cat /usr/local/lib/netstat/logs/$(date -d "24 hours ago" +%F)/dig.*.log | sort -rn -k13,13 | head -n 10 | awk '{print $2,$3,$4,$13,$8,$11}') |  sort -n -k3,3
+ROOT_FOLDER=/usr/local/lib/netstat/logs/$(date -d "24 hours ago" +%F)
+
+(cat $ROOT_FOLDER/fping.*.log | grep ": -" | awk '{print $2,$3,$4,$10,$8}';
+cat $ROOT_FOLDER/fping.*.log | sort -rn -k10,10 | head -n 15 | awk '{print $2,$3,$4,$10,$8}';
+cat $ROOT_FOLDER/dig.*.log | grep 'timed out' | awk '{print $2,$3,$4,$12,$13,$8}';
+cat $ROOT_FOLDER/dig.*.log | sort -rn -k13,13 | head -n 15 | awk '{print $2,$3,$4,$13,$8,$11}') |  sort -n -k3,3
