9eceafc919883448db06bff785a542b1c8bb476f
Jim Wang <jim.wang@corva.ai>
2020-07-26 10:59:43 -0500

    update formatting for log output

--- today_slow.sh
+++ today_slow.sh
@@ -2,7 +2,7 @@
 
 ROOT_FOLDER=/usr/local/lib/netstat/logs/$(date +%F)
 
-(cat $ROOT_FOLDER/fping.*.log | grep ": -" | awk '{print $2,$3,$4,"PING\t",$10,"\t",$8}';
-cat $ROOT_FOLDER/fping.*.log | sort -rn -k10,10 | head -n 15 | awk '{print $2,$3,$4,"PING\t",$10,"\t",$8}';
-cat $ROOT_FOLDER/dig.*.log | grep 'timed out' | awk '{print $2,$3,$4,"DNS\t",$12,"\t",$13,$8}';
-cat $ROOT_FOLDER/dig.*.log | sort -rn -k13,13 | head -n 15 | awk '{print $2,$3,$4,"DNS\t",$13,"\t",$8,$11}') |  sort -n -k3,3
+(cat $ROOT_FOLDER/fping.*.log | grep ": -" | awk '{print $2,$3,$4,"PING\t",$10,"     \t",$8}';
+cat $ROOT_FOLDER/fping.*.log | sort -rn -k10,10 | head -n 15 | awk '{print $2,$3,$4,"PING\t",$10,"     \t",$8}';
+cat $ROOT_FOLDER/dig.*.log | grep 'timed out' | awk '{print $2,$3,$4,"DNS\t",$12,"     \t",$13,$8}';
+cat $ROOT_FOLDER/dig.*.log | sort -rn -k13,13 | head -n 15 | awk '{print $2,$3,$4,"DNS\t",$13,"     \t",$8}') |  sort -n -k3,3
