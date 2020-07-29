c4335da51000f5d4c178ec80d5948a29ccae8d25
FuzzyIO <494127+FuzzyIO@users.noreply.github.com>
2020-07-29 14:43:05 -0500

    Update fping_stat.sh
    Update IP list

--- fping_stat.sh
+++ fping_stat.sh
@@ -4,7 +4,12 @@ ROOT_DIR="/usr/local/lib/netstat/logs/"
 LOG_DIR="$ROOT_DIR$(date +%F)/" && mkdir -p ${LOG_DIR}
 FILE_NAME=${LOG_DIR}/fping.$(date +%F_%H-%M-%S).log
 
-ip_addresses=(4.2.2.1 4.2.2.2 1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4 208.67.222.222 208.67.220.220 68.94.157.1 209.244.0.3)
+## Include Houston Area ONLY IP
+# ip_addresses=(4.2.2.1 4.2.2.2 1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4 208.67.222.222 208.67.220.220 68.94.157.1 209.244.0.3)
+
+## US based IP List
+ip_addresses=(4.2.2.1 4.2.2.2 1.1.1.1 1.0.0.1 8.8.8.8 8.8.4.4 208.67.222.222 208.67.220.220 209.244.0.3)
+
 tmp_file=/tmp/fping_$RANDOM.$RANDOM.log
 
 ## Run this for an hour, ping per each 3 seconds
