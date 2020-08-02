e92ec059e42fa7f417ba5273387dadc504b8f3dd
Jim Wang <jim.wang@corva.ai>
2020-08-02 15:05:12 -0500

    update sort order for logs

--- today_slow.sh
+++ today_slow.sh
@@ -5,4 +5,4 @@ ROOT_FOLDER=/usr/local/lib/netstat/logs/$(date +%F)
 (cat $ROOT_FOLDER/fping.*.log | grep " -" | awk '{print $1,$2,"\t",$4,"     \t",$3}';
 cat $ROOT_FOLDER/fping.*.log | sort -rn -k4,4 | head -n 15 | awk '{print $1,$2,"\t",$4,"     \t",$3}';
 cat $ROOT_FOLDER/dig.*.log | grep " -" | awk '{print $1,$2,"\t",$4,"     \t",$3}';
-cat $ROOT_FOLDER/dig.*.log | sort -rn -k4,4 | head -n 15 | awk '{print $1,$2,"\t",$4,"     \t",$3}';) |  sort -n -k3,3
+cat $ROOT_FOLDER/dig.*.log | sort -rn -k4,4 | head -n 15 | awk '{print $1,$2,"\t",$4,"     \t",$3}';) |  sort -n -k1,1
